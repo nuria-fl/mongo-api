326ae2be3b4e61c593ed0afa56c1986659daedfb
Nuria <soriano.nuria@gmail.com>
2016-08-11 13:11:45 +0200

    filtering results

--- app.js
+++ app.js
@@ -8,14 +8,50 @@ mongo.connect(url, function(err, db) {
 	if (err) throw new Error("oops");
 
 	app.get('/restaurants', function(req,res) {
-		
 		console.log("Connected");
-
 		var collection = db.collection('restaurants');
 
-		collection.find({}).toArray(function(err, docs) {
-  			res.json(docs);
-          	db.close();
+		if(Object.keys(req.query).length === 0){
+
+			collection.find({}).toArray(function(err, docs) {
+				if (err) throw new Error("oops");
+				res.json(docs);
+			});
+			
+		} else {
+
+			var newObj = {};
+
+			if (req.query.show) {
+				var toShow = req.query.show.split(',');
+				toShow.forEach(function(elem){
+					newObj[elem] = 1;
+				});
+			}			
+			if(req.query.hide){
+				var toHide = req.query.hide.split(',');	
+				toHide.forEach(function(elem){
+					newObj[elem] = 0;
+				});
+			}
+			collection.find({}, newObj).toArray(function(err, docs) {
+				if (err) throw new Error("oops");
+				res.json(docs);
+			});
+		}
+	});
+
+	app.get('/restaurants/borough/:borough', function(req,res) {
+		
+		console.log("Connected");
+		var collection = db.collection('restaurants');
+
+		var borough = req.params.borough;
+
+		collection.find({borough: borough}).toArray(function(err, docs) {
+			if (err) throw new Error("oops");
+			res.json(docs);
+			
 		});
 
 	});
